f065c9aaa65608c4d047ffa2cc8e35bb363a3b0a
Hyudz <fraionhyudz10@gmail.com>
2023-12-05 18:40:46 +0800

    Improved UI

---
+++ app/src/main/java/com/esguerra/fraionHyudz/block1/p1/quiz/MainPage.kt
@@ -0,0 +1,15 @@
+package com.esguerra.fraionHyudz.block1.p1.quiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainPage : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
