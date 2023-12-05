e4d5c1720320db070e677ba70ace329053866ee4
Hyudz <fraionhyudz10@gmail.com>
2023-12-05 14:45:23 +0800

    Added new layouts

---
+++ app/src/main/java/com/esguerra/fraionHyudz/block1/p1/quiz/Signup.kt
@@ -0,0 +1,13 @@
+package com.esguerra.fraionHyudz.block1.p1.quiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+
+class Signup : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+    }
